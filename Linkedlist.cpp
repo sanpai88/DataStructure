b612ade49981744757336d1cbabe8bf37733851b
Santhosh Pai <c_spai@edgeq.io>
2024-05-05 13:20:54 +0530

    Indentation of code

--- Linkedlist.cpp
+++ Linkedlist.cpp
@@ -3,68 +3,68 @@
 using namespace std;
 struct node
 {
-    int data;
-    struct node *next;
-    
+	int data;
+	struct node *next;
+
 };
 
 class List{
-    
-    struct node *head;
-    
-    public:
-        List():head(NULL){}
+
+	struct node *head;
+
+	public:
+	List():head(NULL){}
 	~List();
-        void addNode(int data);
-        void displayList();
+	void addNode(int data);
+	void displayList();
 };
 
 List::~List()
 {
-    struct node *headptr;
-    while(head != NULL )
-    {
-         headptr = head;
-	 head = head->next;
-	 delete headptr;
-    }
+	struct node *headptr;
+	while(head != NULL )
+	{
+		headptr = head;
+		head = head->next;
+		delete headptr;
+	}
 }
 
 void List::addNode(int data)
 {
-    struct node *newnode = NULL;
-    if(head == NULL)
-    {
-        newnode = new struct node;
-        newnode->data =  data;
-        newnode->next = NULL;
-        head = newnode;
-    } else {
-        newnode = new struct node;
-        newnode->data =  data;
-        newnode->next = head;
-        head = newnode;
-    }
+	struct node *newnode = NULL;
+	if(head == NULL)
+	{
+		newnode = new struct node;
+		newnode->data =  data;
+		newnode->next = NULL;
+		head = newnode;
+	} else {
+		newnode = new struct node;
+		newnode->data =  data;
+		newnode->next = head;
+		head = newnode;
+	}
 }
 
 void List::displayList()
 {
-    struct node *headptr = head;
-     while(headptr != NULL )
-     {
-         cout<<headptr->data<<endl;
-         headptr = headptr->next;
-     }
+	struct node *headptr = head;
+	while(headptr != NULL )
+	{
+		cout<<headptr->data<<endl;
+		headptr = headptr->next;
+	}
 }
 
 int main() {
-    // Write C++ code here
-    
-    List obj;
-    obj.addNode(1);
-    obj.addNode(2);
-    obj.addNode(3);
+	// Write C++ code here
 
-    obj.displayList();
-    return 0;
+	List obj;
+	obj.addNode(1);
+	obj.addNode(2);
+	obj.addNode(3);
+
+	obj.displayList();
+	return 0;
 }
