79ef3f7cd5a55fede6729efb829c8f48a70c6003
Santhosh Pai <c_spai@edgeq.io>
2024-05-06 20:22:39 +0530

    Indentation improvement

--- Queue.cpp
+++ Queue.cpp
@@ -3,99 +3,109 @@
 using namespace std;
 struct node
 {
-    int data;
-    struct node *next;
-    
+  int data;
+  struct node *next;
 };
 
-class Queue{
-    
-    struct node *head;
-    struct node *tail;
+class Queue
+{
 
-    public:
-        Queue():head(NULL),tail(NULL){}
-        void enQueue(int data);
-        int  deQueue(); 
-	void displayQueue();
+  struct node *head;
+  struct node *tail;
+
+public:
+  Queue () : head (NULL), tail (NULL) {}
+  void enQueue (int data);
+  int deQueue ();
+  void displayQueue ();
 };
 
-void Queue::enQueue(int data)
+void
+Queue::enQueue (int data)
 {
-    struct node *newnode = NULL;
-    if(tail == NULL)
+  struct node *newnode = NULL;
+  if (tail == NULL)
     {
-        newnode = new struct node;
-        newnode->data =  data;
-        newnode->next = NULL;
-        head = newnode;
-	tail = newnode;
-    } else {
-        newnode = new struct node;
-        newnode->data =  data;
-        newnode->next = NULL;
-        tail->next = newnode;
-	tail = newnode;
+      newnode = new struct node;
+      newnode->data = data;
+      newnode->next = NULL;
+      head = newnode;
+      tail = newnode;
+    }
+  else
+    {
+      newnode = new struct node;
+      newnode->data = data;
+      newnode->next = NULL;
+      tail->next = newnode;
+      tail = newnode;
     }
 }
 
-int Queue::deQueue()
+int
+Queue::deQueue ()
 {
-    int val;
-    if( head == tail && head == NULL )
+  int val;
+  if (head == tail && head == NULL)
     {
-        cout<<"Queue is empty";
-	return 0;
-    }else if (head == tail )
+      cout << "Queue is empty";
+      return 0;
+    }
+  else if (head == tail)
     {
-	struct node *tmpnode;
-	//cout<<head->data<<endl;
-	val = head->data;
-	tmpnode = head;
-	delete tmpnode;
-	head = NULL;
-	tail = NULL;
-    } else {
-	struct node *tmpnode;
-	//cout<<head->data<<endl;
-	val = head->data;
-	tmpnode = head;
-        head = head->next;
-	delete tmpnode;
+      struct node *tmpnode;
+      // cout<<head->data<<endl;
+      val = head->data;
+      tmpnode = head;
+      delete tmpnode;
+      head = NULL;
+      tail = NULL;
+    }
+  else
+    {
+      struct node *tmpnode;
+      // cout<<head->data<<endl;
+      val = head->data;
+      tmpnode = head;
+      head = head->next;
+      delete tmpnode;
     }
 
-    return val;
+  return val;
 }
-void Queue::displayQueue()
+void
+Queue::displayQueue ()
 {
-    struct node *headptr = head;
-    if(headptr == NULL )
+  struct node *headptr = head;
+  if (headptr == NULL)
     {
-        cout<<"Queue is empty"<<endl;
-	return;
+      cout << "Queue is empty" << endl;
+      return;
     }
-    while(headptr != NULL )
+  while (headptr != NULL)
     {
-         cout<<headptr->data<<endl;
-         headptr = headptr->next;
+      cout << headptr->data << endl;
+      headptr = headptr->next;
     }
 }
 
-int main() {
-    // Write C++ code here
-    //std::cout << "Try programiz.pro"<<endl;
-    
-    Queue obj;
-    obj.enQueue(1);
-    obj.enQueue(2);
-    obj.enQueue(3);
+int
+main ()
+{
+  // Write C++ code here
+  // std::cout << "Try programiz.pro"<<endl;
 
-    obj.displayQueue();
-    obj.deQueue();
-    obj.deQueue();
-    obj.deQueue();
-  
-    cout<<"After delete"<<endl; 
-    obj.displayQueue();
-    return 0;
+  Queue obj;
+  obj.enQueue (1);
+  obj.enQueue (2);
+  obj.enQueue (3);
+
+  obj.displayQueue ();
+  obj.deQueue ();
+  obj.deQueue ();
+  obj.deQueue ();
+
+  cout << "After delete" << endl;
+  obj.displayQueue ();
+  return 0;
 }
