60d2c45977f19cc3ed426f02a69fa610ac61d510
Santhosh Pai <c_spai@edgeq.io>
2025-03-03 17:25:34 +0530

    Swap bits at n pos

---
+++ swap_bits.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdint.h>
+
+uint32_t swapBits(uint32_t in, uint32_t pos1, uint32_t pos2)
+{
+    uint32_t mask = 0;
+    uint32_t firstPos =  ( in >> pos1 ) & 1; 
+    uint32_t secondPos = ( in >> pos2 ) & 1;
+    //clear bits at respective pos 
+    in  =  ( in & ~( 1 << pos1));
+    in  =  ( in & ~(1 << pos2));
+    mask = (firstPos << pos2 ) | (secondPos << pos1);
+
+    return in | mask;
+}
+
+int main()
+{
+    uint32_t in1 = 4 , in2 = 3 ;
+
+    uint32_t op = swapBits(3,1,2);
+    printf("after swap:%u \n",op);
+
+}
